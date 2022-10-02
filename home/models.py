b1525f5337c953489bb20c05c146c4e31d7f7e57
LuisMoreno <luisjmj.1@gmail.com>
2022-10-02 17:48:55 -0300

    familia

--- home/models.py
+++ home/models.py
@@ -5,3 +5,9 @@ class Persona(models.Model):
     apellido = models.CharField(max_length=20)
     edad = models.IntegerField()
     fecha_nacimiento = models.DateField(null=True)
+
+class Familia(models.Model):
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=20)
+    edad = models.IntegerField()
+    fecha_nacimiento = models.DateField(null=True)
--- miProyecto/views.py
+++ miProyecto/views.py
@@ -6,6 +6,7 @@ from datetime import datetime, timedelta
 from django.template import Context, Template, loader
 import random
 from home.models import Persona
+from home.models import Familia
 
 
 def fecha(request):
@@ -18,14 +19,16 @@ def hola(request):
 
 def crear_familia(request):
     
-    persona1 = Persona(nombre='Lola', apellido='Mento', edad=random.randrange(1, 99), fecha_nacimiento=datetime.now())
-    persona2 = Persona(nombre='Dolores', apellido='Delano', edad=random.randrange(1, 99), fecha_nacimiento=datetime.now())
-    persona3 = Persona(nombre='Pere', apellido='Gil', edad=random.randrange(1, 99), fecha_nacimiento=datetime.now())
-    persona4 = Persona(nombre='kai', apellido='Parada', edad=random.randrange(1, 99), fecha_nacimiento=datetime.now())
-    persona1.save()
-    persona2.save()
-    persona3.save()
-    persona4.save()
+    familia = Familia.objects.all()
+    
+    familia1 = Familia(nombre='Lola', apellido='Mento', edad=random.randrange(1, 99), fecha_nacimiento=datetime.now())
+    familia2 = Familia(nombre='Dolores', apellido='Delano', edad=random.randrange(1, 99), fecha_nacimiento=datetime.now())
+    familia3 = Familia(nombre='Pere', apellido='Gil', edad=random.randrange(1, 99), fecha_nacimiento=datetime.now())
+    familia4 = Familia(nombre='kai', apellido='Parada', edad=random.randrange(1, 99), fecha_nacimiento=datetime.now())
+    familia1.save()
+    familia2.save()
+    familia3.save()
+    familia4.save()
     
     template = loader.get_template('crear_familia.html')
     template_renderizado = template.render({})

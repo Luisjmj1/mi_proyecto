b5a212213fa222cd9c2d83977a53348c8137c35a
LuisMoreno <luisjmj.1@gmail.com>
2022-10-02 15:54:37 -0300

    listo

--- miProyecto/views.py
+++ miProyecto/views.py
@@ -16,17 +16,6 @@ def hola(request):
     return HttpResponse('Holaaaaaaaaaaaaaaaaaaa')
 
 
-
-def crear_persona(request, nombre, apellido):
-    
-   persona = Persona(nombre=nombre, apellido=apellido, edad=random.randrange(1, 99), fecha_nacimiento=datetime.now())
-    
-    template = loader.get_template('crear_persona.html')
-    template_renderizado = template.render({'persona': persona})
-    
-    
-    return HttpResponse(template_renderizado)
-
 def ver_personas(request):
     
     personas = Persona.objects.all()

e2a7be702607ab1afff1532fb73e5a5bc45a7128
LuisMoreno <luisjmj.1@gmail.com>
2022-10-15 16:10:27 -0300

    Cree el suoer usuario

--- home/models.py
+++ home/models.py
@@ -5,9 +5,17 @@ class Persona(models.Model):
     apellido = models.CharField(max_length=20)
     edad = models.IntegerField()
     fecha_nacimiento = models.DateField(null=True)
+    
+    def __str__(self):
+        return f'{self.nombre} {self.apellido}'
+        
 
 class Familia(models.Model):
     nombre = models.CharField(max_length=20)
     apellido = models.CharField(max_length=20)
     edad = models.IntegerField()
     fecha_nacimiento = models.DateField(null=True)
+    
+    def __str__(self):
+        return f'{self.nombre} {self.apellido}'
+    
